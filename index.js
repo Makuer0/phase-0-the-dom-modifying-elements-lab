676fe900a1a4ab97321731079e227541df6ed469
Makuer <makuer.mqtur@student.moringaschool.com>
2025-03-02 12:36:54 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+
+// Create an h1 element
+const newHeader = document.createElement("h1");
+
+// Set its id attribute to "victory"
+newHeader.id = "victory";
+
+// Add text content to the h1
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append it to the body
+document.body.appendChild(newHeader);
